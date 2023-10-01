a013948f72444be82cd5dcad9b8d81dffc3f51ab
jessegestin <jessegestin@gmail.com>
2023-10-01 19:26:52 -0300

    Commit inicial

---
+++ divulgar/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('novo_registro/', views.novo_registro, name="novo_registro"),
+    path('seus_registros/', views.seus_registros, name="seus_registros"),
+    path('remover_registro/<int:id>', views.remover_registro, name="remover_registro"),
+    path('ver_registro/<int:id>', views.ver_registro, name="ver_registro"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('api_adocoes_por_raca/', views.api_adocoes_por_raca, name="api_adocoes_por_raca"),
+]
